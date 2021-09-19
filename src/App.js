e1403be92fcae9f5aebdafbc5056b9361d62b4d1
jonrutter <contact@jonrutter.com>
2021-09-19 12:31:27 -0400

    CLear unused code from app entry point

--- src/App.js
+++ src/App.js
@@ -1,42 +1,8 @@
 import React from 'react';
 import QuoteBox from './components/QuoteBox';
 
-import { randomNum } from './helpers';
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      themeColor: '#000',
-    };
-    this.themeChange = this.themeChange.bind(this);
-  }
-
-  themeChange() {
-    this.setState({
-      themeColor: `rgb(${randomNum(0, 255)}, ${randomNum(0, 255)}, ${randomNum(
-        0,
-        255
-      )})`,
-    });
-  }
-
-  render() {
-    return (
-      <div
-        className="app"
-        style={{
-          backgroundColor: this.state.themeColor,
-          color: this.state.themeColor,
-        }}
-      >
-        <QuoteBox
-          themeColor={this.state.themeColor}
-          themeChange={this.themeChange}
-        />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return <QuoteBox />;
+};
 
 export default App;
